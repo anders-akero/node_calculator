9404a47f118161ac38adb69aab0f39699ee73163
Anders Åkerö <anders.akero@motionpicturesolutions.com>
2018-05-13 19:13:43 +0100

    Creating Calculator class

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ const express = require('express');
 const app = express();
 const prefix = 'Result: ';
 
-app.use('/add', function(req, res){
-  const args =  req.path.substring(1).split('/');
-  let result = 0;
-  for (const arg of args) {
-    result += parseInt(arg);
-  }
-  res.send(prefix + result);
+app.use('/add', function (req, res) {
+    const args = req.path.substring(1).split('/');
+    const calc = new Calculator();
+    res.send(prefix + calc.add(args));
 });
 
 app.listen(3000);
+
+class Calculator {
+    constructor(sum = 0) {
+        this.sum = sum;
+    }
+
+    add(args) {
+        for (const arg of args) {
+            this.sum += parseInt(arg);
+        }
+        return this.sum;
+    }
+}
